c97df262dcfbc3cd8358103635af6d24e456b6b3
Cristian Omar Jiménez Sánchez <cristianomar71@outlook.com>
2025-09-23 22:48:46 -0600

    Mostrando ordenes nuevas y listando las ordenes ya completadas en su pantalla aparte

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <h1>Hola mundo en next.js</h1>
-  );
+  redirect('/order/cafe')
 }
--- src/store.ts
+++ src/store.ts
@@ -5,23 +5,21 @@ import { Product } from "@prisma/client"
 interface Store {
     order: OrderItem[]
     addToOrder: (product: Product) => void
-    increaseQuantity : (id: Product['id']) => void
-    decreaseQuantity : (id: Product['id']) => void
-    removeItem : (id: Product['id']) => void
-    clearOrder : () => void
+    increaseQuantity: (id: Product['id']) => void
+    decreaseQuantity: (id: Product['id']) => void
+    removeItem: (id: Product['id']) => void
+    clearOrder: () => void
 }
 
 export const useStore = create<Store>((set, get) => ({
     order: [],
     addToOrder: (product) => {
-
         const { categoryId, image, ...data } = product
         let order: OrderItem[] = []
-
         if (get().order.find(item => item.id === product.id)) {
-            order = get().order.map( item => item.id === product.id ? {
+            order = get().order.map(item => item.id === product.id ? {
                 ...item,
-                quantity : item.quantity + 1,
+                quantity: item.quantity + 1,
                 subtotal: item.price * (item.quantity + 1)
             } : item)
         } else {
@@ -31,42 +29,41 @@ export const useStore = create<Store>((set, get) => ({
                 subtotal: 1 * product.price
             }]
         }
+        set(() => ({
+            order
+        }))
+    },
+    increaseQuantity: (id) => {
+        set((state) => ({
+            order: state.order.map(item => item.id === id ? {
+                ...item,
+                quantity: item.quantity + 1,
+                subtotal: item.price * (item.quantity + 1)
+            } : item)
+        }))
+    },
+    decreaseQuantity: (id) => {
+
+        const order = get().order.map(item => item.id === id ? {
+            ...item,
+            quantity: item.quantity - 1,
+            subtotal: item.price * (item.quantity - 1)
+        } : item)
 
         set(() => ({
             order
         }))
     },
-     increaseQuantity :(id) => {
-         set((state) => ({
-            order : state.order.map( item => item.id === id ? {
-                ...item,
-                quantity : item.quantity + 1,
-                subtotal: item.price * (item.quantity + 1)
-            } : item)
-         }))
-     },
-     decreaseQuantity: (id) => {
-        
-        const order = get().order.map( item => item.id === id ? {
-            ...item,
-            quantity: item.quantity - 1,
-            subtotal: item.price * (item.quantity -1)
-        } : item )
 
-        set(() => ({
-            order
-        })) 
-     },
-
-     removeItem : (id) => {
-         set((state) => ({
+    removeItem: (id) => {
+        set((state) => ({
             order: state.order.filter(item => item.id !== id)
-         }))
-     },
+        }))
+    },
 
-     clearOrder: () => {
+    clearOrder: () => {
         set(() => ({
             order: []
         }))
-     }
+    }
 }))
